0c2f58a28da7fb5e755eed49c7ee8014568cb601
Andrea MacGown <38043829+amacgown00@users.noreply.github.com>
2025-12-21 14:33:13 -0500

    Update hover.js

--- scripts/hover.js
+++ scripts/hover.js
@@ -1,2 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const hovers = document.querySelectorAll("hover");
+	hovers.forEach(el => {
+		let bubble;
+		const showDisplay = () => {
+			bubble = document.createElement("div");
+			bubble.className = "display-bubble";
+			bubble.textContent = el.dataset.display;
+			el.appendChild(bubble);
