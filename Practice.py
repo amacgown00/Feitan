f4f93ad86c51c1a80f7e4e3082a0ca70e28b2563
Andrea L MacGown <amacgown@Andreas-MacBook-Pro.local>
2026-01-26 01:04:57 -0500

    updating

--- Practice.py
+++ Practice.py
@@ -13,5 +13,15 @@ cost = "3"
 
 orchidaceae = ["paphiopedilum", "vanda", "cattleya", "phalaenopsis", "catasetum", "bulbophyllum", "dendrobium"]
 
+for index, column_header in enumerate(orchidaceae):
+    print(index, column_header)
+    
+for cat in orchidaceae:
+    if any(letter in cat for letter in x):
+        print(cat)
 
 num_list = [2, 4, 6, 1, 139, 421, 52, 1738]
+
+for digit in num_list:
+    if digit > 20:
+        print(digit)
