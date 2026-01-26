8a3c3479361651a9eceef2093b9e7f5400bcb00e
Andrea L MacGown <amacgown@Andreas-MacBook-Pro.local>
2026-01-25 19:33:35 -0500

    updating practice

---
+++ Practice.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+text = "Hi Steve Buschemi"
+print(text)
+text = "Hi octopus"
+print(text)
+
+drink, ice = ("passionfruit", "70")
+
+print("I'll have a " + drink.title() + " boba with " + ice + "% ice.")
+
+price = "4 "
+cost = "3"
+
+orchidaceae = ["paphiopedilum", "vanda", "cattleya", "phalaenopsis", "catasetum", "bulbophyllum", "dendrobium"]
+
+
+num_list = [2, 4, 6, 1, 139, 421, 52, 1738]
