105b6a9c1ec5660a070b33b54cb4e2a1f2cac617
Andrea MacGown <38043829+amacgown00@users.noreply.github.com>
2026-01-30 10:32:07 -0500

    Update keccak-256.py

--- 01-cryptography/keccak-256/keccak-256.py
+++ 01-cryptography/keccak-256/keccak-256.py
@@ -6,21 +6,24 @@ print("Input:", message)
 print("Keccak-256:", digest.hex())
 
 
-#This shows how to implement the Keccak-256 algorithm from 
-#the eth_hash library so you don't have to download an Ethereum client. 
+"""
+This shows how to implement the Keccak-256 algorithm from 
+the eth_hash library so you don't have to download an Ethereum client.
 
-#Ethereum uses Keccak-256, an algorithm that creates hashes for:
-#Addresses
-#Transactions
-#Public and private keys
-#Blocks
 
-#Keccak-256 is different from SHA3-256. 
+Ethereum uses Keccak-256, an algorithm that creates hashes for:
+- Addresses
+- Transactions
+- Public and private keys
+- Blocks
 
-#Ethereum started using Keccak before SHA-3 was finalized and continues to use it for consistency.  
+- Keccak-256 is different from SHA3-256. 
 
-#Protocol documentation 
-#What it is
-#Why it exists
-#How it's used
-#what output looks like 
+- Ethereum started using Keccak before SHA-3 was finalized and continues to use it for consistency.  
+
+- Protocol documentation 
+- What it is
+- Why it exists
+- How it's used
+- what output looks like 
+"""
