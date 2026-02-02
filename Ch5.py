e98b3db853983a232dd7e1a1a863905c53e8b5e8
Andrea L MacGown <amacgown@Andreas-MacBook-Pro.local>
2026-02-01 20:23:55 -0500

    Ch. 5

---
+++ Ch5.py
@@ -0,0 +1,12 @@
+shows = ['Hunter x Hunter', 'Death Note', 'Naruto', 'One Piece', 'Jujutsu Kaisen']
+new_show = 'Dandadan'
+if new_show not in shows:
+    print(new_show + " is on my watch list.")
+
+#5-1
+my_cat= 'Quigley'
+print('Is one_cat == "Quigley"? I predict True.')
+print(my_cat== 'Quigley')
+
+print('\nIs my cat == "Chamkila"? I predict false.')
+print(my_cat == 'Chamkila')
