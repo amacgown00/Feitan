9504b7e1989e37a1890d32331ebb9654ed931d69
Andrea L MacGown <amacgown@Andreas-MacBook-Pro.local>
2026-02-02 20:14:33 -0500

    updating ch5.py

--- Ch5.py
+++ Ch5.py
@@ -10,3 +10,68 @@ print(my_cat== 'Quigley')
 
 print('\nIs my cat == "Chamkila"? I predict false.')
 print(my_cat == 'Chamkila')
+
+#2 false because case sensitive
+brand = 'Hermes'
+print("Is brand == 'Hermes'? I predict False")
+print(brand == 'hermes')
+
+#3 
+color = 'pink'
+print("Is my sweatshirt color == 'pink'? I predict True")
+print(color == 'pink')
+
+#4 
+my_age = 29
+your_age = 30
+min_age = 29
+age_test = (my_age and your_age)
+print('We can go if one of us is at least 29')
+print(age_test > min_age)
+
+print('\n')
+
+#5 Can I go to the party? 
+dad = "yes"
+mom = "No"
+print('You can only go to the party if both parents say yes.\n')
+if (mom and dad).lower() == 'yes':
+    print('You can go to the party.')
+    print('We get the coolest parents of the year award')
+else:
+    print("You can't go to the party.")
+
+print('\n\n') 
+
+#6 
+
+
+
+
+
+
+
+
+
+"""
+me = "Me: "
+boy = "Boy: "
+typical_boy_response = ['Only if you want', "Don't leave early, I don't want to interrupt", "I can't call, I'm gaming"]
+recover = "But I can"
+reschedule = ['tomorrow', 'later tonight', 'this weekend']
+his_actual_response = 'Only if you want'
+
+print(me + 'Can we call tonight?')
+print(boy + his_actual_response)
+if his_actual_response in typical_boy_response:
+    print(me + "Let's just not call then")
+"""
+
+
+#if you ask a boy to call and he says 1, 2, 3, and doesn't add a reschedule, then you 
+
+
+
+
+
+
