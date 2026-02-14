e3c3e9340664a9b3b056f2f5fc3c2b0167f0628e
ALM <amacgown00@gmail.com>
2026-02-14 15:44:52 -0500

    updating ch6

--- Ch6.py
+++ Ch6.py
@@ -16,5 +16,39 @@ print(hxh_char['Kurapika'])
 kurta_eyes = 'red'
 if kurta_eyes == 'red':
     hxh_char['Kurapika'] = 'specialist'
-    print("When Kurapika's eyes are " + kurta_eyes + ", he becomes a " + hxh_char['Kurapika'] + '.')
-    
+    print("When Kurapika's eyes are " + kurta_eyes + ", he becomes a " + hxh_char['Kurapika'] + '.\n\n\n')
+
+space = '\n\n'
+
+friend = {
+    'first_name' : 'Sahar',
+    'last_name' : 'Aftab',
+    'age' : '29',
+    'city' : 'Yonkers',   
+}
+print(friend['first_name'] +
+      " " +
+      friend['last_name'] +
+      ' ' +
+      friend['age'] + 
+      ' ' + 
+      friend['city'] + '\n'
+      )
+
+friend_numbers = {
+    'sahar' : '24',
+    'jeny' : '73',
+    'hannah' : '36',
+    'mike' : '',
+}
+
+friend_numbers['mike'] = (17 ** 7) % 287 
+
+print("Mike's favorite number is: " + str(friend_numbers['mike']))
+
+print(space)
+
+new_mike_number = 29
+friend_numbers['mike'] = new_mike_number
+
+print(friend_numbers['mike'])
